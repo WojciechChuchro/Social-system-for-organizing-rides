b7dcd746d61f62670280dcbc56542378f9076a8f
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-07-23 21:11:45 +0200

    Dockerize symfony project.

--- server/config/routes/Authentication.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controller\AuthenticationController;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-
-return function (RoutingConfigurator $routes): void {
-	$routes->add('login', '/api/login')->controller([AuthenticationController::class, 'login']);
-	$routes->add('register', '/api/register')->controller([AuthenticationController::class, 'register']);
-};
--- server/config/routes/Users.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controller\UsersController;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-
-return function (RoutingConfigurator $routes): void {
-	$routes->add('users', '/users')->controller([UsersController::class, 'getEveryUser']);
-	$routes->add('userById', '/userById')->controller([UsersController::class, 'getSingleUser']);
-};
