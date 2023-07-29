e6c14136bc5169f1436c1513d9ad587002bcdb2f
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-07-29 20:05:39 +0200

    Dockerize project v2.

--- server/src/main/java/com/ssfor/server/ServerApplication.java
+++ server/src/main/java/com/ssfor/server/ServerApplication.java
@@ -3,7 +3,7 @@ package com.ssfor.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+	@SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
