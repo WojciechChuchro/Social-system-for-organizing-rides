dd0ddcd2c6a80d84438c5d99fb2693c34f339a13
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-08-07 06:58:35 +0200

    Create dummy data using faker.

--- server/src/database/migrations/20230806113735_users.ts
+++ server/src/database/migrations/20230806113735_users.ts
@@ -5,6 +5,10 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
         table.increments('id').primary();
         table.string('email').notNullable().unique();
+        table.string('name')
+        table.string('surname')
+        table.string('phoneNumber')
+        table.string('profilePicture')
         table.string('password').notNullable();
         table.string('salt');
         table.string('sessionToken');
--- server/src/database/seeds/01_users.ts
+++ server/src/database/seeds/01_users.ts
@@ -1,10 +1,23 @@
-import { Knex } from "knex";
+import {Knex} from "knex";
+// @ts-ignore
+import * as faker from "faker";
 
 export async function seed(knex: Knex): Promise<void> {
+    const users = [];
+    const numberOfUsers = 10;
+
+    for (let i = 0; i < numberOfUsers; i++) {
+        users.push({
+            email: faker.internet.email(),
+            name: faker.name.firstName(),
+            surname: faker.name.lastName(),
+            phoneNumber: faker.phone.phoneNumber(),
+            profilePicture: faker.internet.avatar(),
+            password: faker.internet.password(),
+            salt: faker.random.alphaNumeric(16),
+            sessionToken: faker.datatype.uuid(),
+        });
+    }
     await knex("users").del();
-    await knex("users").insert([
-        { id: 1, email: "email1", password: "password1" },
-        { id: 2, email: "email2", password: "password2" },
-        { id: 3, email: "email3", password: "password3" }
-    ]);
-};
+    await knex("users").insert(users);
+}
