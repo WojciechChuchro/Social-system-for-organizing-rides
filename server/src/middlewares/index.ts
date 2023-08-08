e63c5616e1f01625aa540f57bdbd5ff6c34add9f
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-08-08 07:23:05 +0200

    Delete unused middleware.

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -22,26 +22,3 @@ export const authenticateJWT = (req: Request, res: Response, next: NextFunction)
 
     next();
 };
-export const isOwner = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const {id} = req.params
-        const currentUserId = get(req, "identity._id") as string
-
-        if (!currentUserId) {
-            return res.sendStatus(400)
-        }
-
-        if (currentUserId.toString() !== id) {
-            return res.sendStatus(403)
-        }
-
-        next()
-    } catch (error) {
-        console.log(error)
-        return res.sendStatus(400)
-    }
-}
