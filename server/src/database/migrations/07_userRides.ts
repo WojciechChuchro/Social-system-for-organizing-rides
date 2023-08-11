784aa39611f65ce71dfc68bf46f51cd140687b93
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-08-11 07:29:44 +0200

    Create/update userRide model/seed/migration.

---
+++ server/src/database/migrations/07_userRides.ts
@@ -0,0 +1,37 @@
+import { Knex } from "knex";
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('userRides', (table: Knex.TableBuilder) => {
+        table.increments('id').primary();
+
+        table.integer("userId")
+            .unsigned()
+            .references("id")
+            .inTable("users")
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table.integer("lookingForDriverId")
+            .unsigned()
+            .references("id")
+            .inTable("lookingForDrivers")
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table.integer("rideId")
+            .unsigned()
+            .references("id")
+            .inTable("rides")
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table.integer("StatusId")
+            .unsigned()
+            .references("id")
+            .inTable("statuses")
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('userRides');
+}
---
+++ server/src/database/seeds/07_userRides.ts
@@ -0,0 +1,25 @@
+import {Knex} from "knex";
+import {faker} from '@faker-js/faker';
+
+export async function seed(knex: Knex): Promise<void> {
+    await knex("userRides").del();
+
+    const userRides = [];
+    const numberOfUserRides = 10;
+
+    const existingStatusesIds = await knex("statuses").pluck("id");
+    const existingUsersIds = await knex("users").pluck("id");
+    const existingLookingForDriversIds = await knex("lookingForDrivers").pluck("id");
+    const existingRidesIds = await knex("rides").pluck("id");
+
+    for (let i = 0; i < numberOfUserRides; i++) {
+        userRides.push({
+            userId: existingUsersIds[faker.number.int({min: 0, max: 9})],
+            rideId: existingRidesIds[faker.number.int({min: 0, max: 9})],
+            statusId: existingStatusesIds[faker.number.int({min: 0, max: 9})],
+            lookingForDriverId: existingLookingForDriversIds[faker.number.int({min: 0, max: 9})],
+        });
+    }
+
+    await knex("userRides").insert(userRides);
+}
