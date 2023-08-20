bde5a7cc580e0568e59a07408b60ecf55917a33b
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-08-20 19:05:49 +0200

    Create new components and add them to the routing.

---
+++ client/src/app/your-rides/your-rides.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-your-rides',
+  templateUrl: './your-rides.component.html',
+  styleUrls: ['./your-rides.component.scss']
+})
+export class YourRidesComponent {
+
+}
