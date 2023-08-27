4cee9d3d1d41af575b0975398600e5efd4e43738
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-08-27 09:29:01 +0200

    Create simple guard test.

--- client/src/app/guards/auth.guard.spec.ts
+++ client/src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,44 @@
-import { TestBed } from '@angular/core/testing'
-import { CanActivateFn } from '@angular/router'
+import {TestBed} from '@angular/core/testing'
+import {RouterTestingModule} from '@angular/router/testing'
+import {AuthGuard} from './auth.guard'
+import {AuthService} from '../../services/auth.service'
+import {Router} from '@angular/router'
+import {of} from 'rxjs'
 
-import { authGuard } from './auth.guard'
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-    TestBed.runInInjectionContext(() => authGuard(...guardParameters))
+describe('AuthGuard', () => {
+  let guard: AuthGuard
+  let authService: jasmine.SpyObj<AuthService>
+  let router: Router
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    const authSpy = jasmine.createSpyObj('AuthService', ['validateToken'])
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        AuthGuard,
+        {provide: AuthService, useValue: authSpy}
+      ],
+    })
+    guard = TestBed.inject(AuthGuard)
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>
+    router = TestBed.inject(Router)
   })
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy()
+    expect(guard).toBeTruthy()
+  })
+
+  describe('canActivate', () => {
+    it('should return true if the user is authenticated', () => {
+      authService.validateToken.and.returnValue(of(true))
+      expect(guard.canActivate()).toBeTrue()
+    })
+
+    it('should navigate to login if the user is not authenticated', () => {
+      const navigateSpy = spyOn(router, 'navigate')
+      authService.validateToken.and.returnValue(of(false))
+      expect(guard.canActivate()).toBeFalse()
+      expect(navigateSpy).toHaveBeenCalledWith(['/login'])
+    })
   })
 })
