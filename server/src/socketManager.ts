40b74a819d9acb531ee8e5358dd0a9617f8c269e
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-03 21:23:13 +0200

    Refactor: delete unnecessary comments

--- server/src/socketManager.ts
+++ server/src/socketManager.ts
@@ -1,43 +1,31 @@
 import {Server, Socket} from 'socket.io'
-// const userToSocket = new Map();
-//
-// io.on('connection', (socket) => {
-//   console.log('New client connected')
-//   socket.on('register', (userId) => {
-//     userToSocket.set(userId, socket.id)
-//   })
-//
-//   // Custom event for sending a message
-//   socket.on('sendMessage', (data) => {
-//     const targetSocketId = userToSocket.get(data.targetUserId)
-//     if (targetSocketId) {
-//       io.to(targetSocketId).emit('newMessage', data)
-//     }
-//   })
-//
-//   socket.on('disconnect', () => {
-//     // Remove the Socket ID when the user disconnects
-//     userToSocket.delete(socket.id)
-//     console.log('Client disconnected')
-//   })
-//   // Handle custom events or whatever you need
-//   socket.on('sendMessage', (data) => {
-//     console.log('sendMessage triggered:', data)
-//
-//     // Emit the message to all connected clients
-//     io.emit('newMessage', data)
-//   })
-// })
+
 export const configureSocket = (io: Server) => {
+  const userToSocket = new Map()
+
   io.on('connection', (socket: Socket) => {
     console.log('New client connected')
+    socket.on('register', (userId) => {
+      userToSocket.set(userId, socket.id)
+    })
 
     socket.on('sendMessage', (data) => {
-      console.log('Message received:', data)
+      const targetSocketId = userToSocket.get(data.targetUserId)
+
+      if (targetSocketId) {
+        io.to(targetSocketId).emit('newMessage', data)
+      }
     })
 
     socket.on('disconnect', () => {
+      userToSocket.delete(socket.id)
       console.log('Client disconnected')
+    })
+
+    socket.on('sendMessage', (data) => {
+      console.log('sendMessage triggered:', data)
+
+      io.emit('newMessage', data)
     })
   })
 }
