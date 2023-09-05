4d66c2be5a8d5ea2186e8c11f0f13dd19369a5fa
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-05 21:03:12 +0200

    Refactor: better displaying of errors and make better syntax for fetch

--- client/src/app/your-rides/your-rides.component.ts
+++ client/src/app/your-rides/your-rides.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
 import {Rides, UserRides} from '../../types/response'
-import {RideService, UserRides2} from '../../services/ride.service'
+import {RideService} from '../../services/ride.service'
 import {MessageResponseOnly} from '../../types/user'
 import {MatSnackBar} from '@angular/material/snack-bar'
+import {UtilityService} from '../../services/utility.service'
 
 @Component({
   selector: 'app-your-rides',
@@ -12,43 +13,64 @@ import {MatSnackBar} from '@angular/material/snack-bar'
 })
 export class YourRidesComponent implements OnInit {
   rides: Rides[] = []
-  ridesPassangers: UserRides2[] = []
+  ridesPassangers: UserRides[] = []
 
-  constructor(private http: HttpClient, private rideService: RideService, private snackBar: MatSnackBar,) {
+  constructor(private http: HttpClient, private rideService: RideService, private snackBar: MatSnackBar, private utilityService: UtilityService) {
   }
 
-  showAlert(message: string, action: string, duration: number): void {
-    this.snackBar.open(message, action, {
-      duration: duration,
-    })
-  }
+  // showAlert(message: string, action: string, duration: number): void {
+  //   this.snackBar.open(message, action, {
+  //     duration: duration,
+  //   })
+  // }
 
   ngOnInit(): void {
     this.fetchAllRides()
-    console.log('ridesPassanger',this.ridesPassangers)
   }
 
   fetchAllRides(): void {
-    this.rideService.fetchRides().subscribe((rides: Rides[]) => {
-      this.rides = rides
+    this.rideService.fetchRides().subscribe({
+      next: (rides: Rides[]) => {
+        this.rides = rides
+      },
+      error: error => {
+        const errorMessage = error.error.message || 'An unknown error occurred'
+        this.utilityService.showAlert(errorMessage, 'Close', 3000)
+      },
     })
 
-    this.rideService.fetchRidesAsPassenger().subscribe((rides: UserRides2[]) => {
-      this.ridesPassangers = rides
+    this.rideService.fetchRidesAsPassenger().subscribe({
+      next: (rides: UserRides[]) => {
+        this.ridesPassangers = rides
+      },
+      error: error => {
+        const errorMessage = error.error.message || 'An unknown error occurred'
+        this.utilityService.showAlert(errorMessage, 'Close', 3000)
+      },
     })
   }
-
   deleteUser(statusId: number): void {
-    this.rideService.deleteUser(statusId).subscribe((response: MessageResponseOnly) => {
-      this.showAlert(response.message, 'Close', 3000)
-      this.fetchAllRides()
+    this.rideService.deleteUser(statusId).subscribe({
+      next: (response: MessageResponseOnly) => {
+        this.utilityService.showAlert(response.message, 'Close', 3000)
+        this.fetchAllRides()
+      },
+      error: error => {
+        const errorMessage = error.error.message || 'An unknown error occurred'
+        this.utilityService.showAlert(errorMessage, 'Close', 3000)
+      }
     })
   }
-
   acceptUser(statusId: number): void {
-    this.rideService.acceptUser(statusId).subscribe((response: MessageResponseOnly) => {
-      this.showAlert(response.message, 'Close', 3000)
-      this.fetchAllRides()
+    this.rideService.acceptUser(statusId).subscribe({
+      next: (response: MessageResponseOnly) => {
+        this.utilityService.showAlert(response.message, 'Close', 3000)
+        this.fetchAllRides()
+      },
+      error: error => {
+        const errorMessage = error.error.message || 'An unknown error occurred'
+        this.utilityService.showAlert(errorMessage, 'Close', 3000)
+      }
     })
   }
 }
