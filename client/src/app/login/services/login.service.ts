ba8eed704075bce9d1e4c59f807afad240c5886f
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-05 21:53:39 +0200

    Refactor: delete useless comments

--- client/src/app/login/services/login.service.ts
+++ client/src/app/login/services/login.service.ts
@@ -19,8 +19,6 @@ export class LoginService {
     return this.http.post<MessageResponseOnly>('http://localhost:8000/api/login', requestBody)
       .subscribe({
         next: (response: MessageResponseOnly) => {
-          // If you want to perform some operations with the response, you can do so here.
-          // For now, we'll simply return it.
           return response.message
         },
         error: (error) => {
