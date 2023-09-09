cc4738f0a19490409a11a3f5246a840ef13336f1
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-09 18:37:02 +0200

    Change post to get in logout route

--- server/src/router/authentication.ts
+++ server/src/router/authentication.ts
@@ -4,6 +4,6 @@ import express from 'express'
 export default (router: express.Router) => {
   router.post('/auth/register', register)
   router.post('/auth/login', login)
-  router.post('/auth/logout', logout)
+  router.get('/auth/logout', logout)
   router.get('/auth/validate-jwt', validateJWT)
 }
