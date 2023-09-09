3a471c578b77d7088e08b625b3f6d258712cd3a3
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-09 19:33:50 +0200

    Update error showing and make router handling

--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core'
-import {MatSnackBar} from '@angular/material/snack-bar'
 import {AuthService} from '../../services/auth.service'
 import {RegisterForm} from '../../types/user'
 import {UtilityService} from '../../services/utility.service'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-registration',
@@ -20,26 +20,29 @@ export class RegistrationComponent {
   repeatPassword: string = ''
 
   loading: boolean = false
-
-  constructor(private snackBar: MatSnackBar, private auth: AuthService, private utilityService: UtilityService) {
-
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private utilityService: UtilityService,
+  ) {
   }
-
   handleRegister(): void {
     if (this.registerForm.password !== this.repeatPassword) {
-      this.utilityService.showAlert('Passwords don\'t match.', 'Close', 3000);
+      this.utilityService.showAlert('Passwords don\'t match.', 'Close', 3000)
       return
     }
 
     this.loading = true
-    this.auth.register(this.registerForm).subscribe({
+    this.authService.register(this.registerForm).subscribe({
       next: (response) => {
-        this.utilityService.showAlert(response.message, 'Close', 3000);
+        this.utilityService.showAlert(response.message, 'Close', 3000)
         this.loading = false
+        this.router.navigate(['/login'])
       },
       error: (error) => {
         this.loading = false
-        this.utilityService.showAlert(error.message, 'Close', 3000);
+        const errorMessage = error.error.message || 'An unknown error occurred'
+        this.utilityService.showAlert(errorMessage, 'Close', 3000)
       },
     })
   }
