d06b3a867fb3bcfe767224571847152e118e7aca
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-09 18:29:28 +0200

    Create default value

--- server/src/database/migrations/03_users.ts
+++ server/src/database/migrations/03_users.ts
@@ -9,16 +9,19 @@ export async function up(knex: Knex): Promise<void> {
       .inTable('cars')
       .onUpdate('CASCADE')
       .onDelete('CASCADE')
+      .defaultTo(null)
+      .nullable()
     table.string('email', 50).notNullable().unique()
     table.string('name', 20).notNullable()
     table.string('surname', 30).notNullable()
     table.string('phoneNumber', 12).notNullable().unique()
-    table.string('profilePicture', 255).nullable()
+    table.string('profilePicture', 255).nullable().defaultTo('default')
     table.string('password', 255).notNullable().notNullable()
     table.string('salt', 255).notNullable()
-    table.string('sessionToken', 255).notNullable()
+    table.string('sessionToken', 255).nullable().defaultTo(null)
   })
 }
+
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.dropTable('users')
 }
