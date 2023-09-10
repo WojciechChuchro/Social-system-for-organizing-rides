a863ec5ad1fd5be7844a5fe62744a994777b20fd
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-10 18:36:22 +0200

    Fix: typo

--- server/src/controllers/statuses.ts
+++ server/src/controllers/statuses.ts
@@ -25,13 +25,12 @@ export const deletePassenger = async (req: Request, res: Response) => {
   const {statusId} = req.body
   try {
 
-    // Update the isAccepted field to 1
     const status = await Statuses.query().patch({ isAccepted: 0 }).findById(statusId)
     if (!status) {
       return res.status(404).json({ message: 'Status not found' })
     }
 
-    return res.status(200).json({ message: 'Passenger accepted successfully' })
+    return res.status(200).json({ message: 'Passenger deleted successfully' })
 
   } catch (err) {
     console.log(err)
