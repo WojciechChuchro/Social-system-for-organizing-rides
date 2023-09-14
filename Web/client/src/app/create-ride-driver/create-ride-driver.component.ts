1d59aaa081e917e5c7112209e65decbed3025ba3
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-14 20:52:50 +0200

    Create dropdown for destination and start city

--- Web/client/src/app/create-ride-driver/create-ride-driver.component.ts
+++ Web/client/src/app/create-ride-driver/create-ride-driver.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { debounceTime, switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-ride-driver',
@@ -12,7 +18,8 @@ export class CreateRideDriverComponent implements OnInit {
   rideForm!: FormGroup;
   cities: string[] = []; // Initialize as an empty array
   showCityList = false;
-
+  destinationCities: string[] = [];
+  showDestinationCityList = false;
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
@@ -27,7 +34,11 @@ export class CreateRideDriverComponent implements OnInit {
       car_model: [''],
       registration_number: [''],
       number_of_seats: [''],
-      start_city: ['', Validators.required],
+      start_city: ['', [Validators.required, this.cityValidator(this.cities)]],
+      destination_city: [
+        '',
+        [Validators.required, this.cityValidator(this.destinationCities)],
+      ],
     });
 
     this.rideForm
@@ -38,18 +49,65 @@ export class CreateRideDriverComponent implements OnInit {
         ),
       )
       .subscribe((data) => {
-        this.cities = data.filteredCities; // Assuming the server returns an object where the cities are in a 'cities' field
+        this.cities = data.filteredCities;
         this.showCityList = true;
+        this.rideForm
+          .get('start_city')
+          ?.setValidators([
+            Validators.required,
+            this.cityValidator(this.cities),
+          ]);
+        this.rideForm
+          .get('start_city')
+          ?.updateValueAndValidity({ emitEvent: false });
+      });
+
+    this.rideForm
+      .get('destination_city')
+      ?.valueChanges.pipe(
+        switchMap((value) =>
+          this.http.get<any>(`http://localhost:8080/api/get-cities/${value}`),
+        ),
+      )
+      .subscribe((data) => {
+        this.destinationCities = data.filteredCities;
+        this.showDestinationCityList = true;
+        this.rideForm
+          .get('destination_city')
+          ?.setValidators([
+            Validators.required,
+            this.cityValidator(this.destinationCities),
+          ]);
+        this.rideForm
+          .get('destination_city')
+          ?.updateValueAndValidity({ emitEvent: false });
       });
   }
 
+  cityValidator(
+    allowedCities: string[],
+  ): (control: AbstractControl) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!allowedCities.includes(value)) {
+        return { cityNotInList: true };
+      }
+      return null;
+    };
+  }
+
   onCitySelected(city: string) {
-    this.rideForm.get('start_city')?.setValue(city);
-    this.cities = []; // Clear the dropdown list
+    this.rideForm.get('start_city')?.setValue(city, { emitEvent: false });
+    this.cities = [];
     this.showCityList = false;
   }
 
+  onDestinationCitySelected(city: string) {
+    this.rideForm.get('destination_city')?.setValue(city, { emitEvent: false });
+    this.destinationCities = [];
+    this.showDestinationCityList = false;
+  }
   handleCreateRide() {
-    // Your POST API logic here
+    console.log(this.rideForm);
   }
 }
