8c276548cde91d94985524040f32826455fc5779
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-09-26 20:09:16 +0200

    Create root user after seeds

--- Web/server/src/database/seeds/03_users.ts
+++ Web/server/src/database/seeds/03_users.ts
@@ -21,6 +21,18 @@ export async function seed(knex: Knex): Promise<void> {
     });
   }
 
+  users.push({
+    carId: existingCarsIds[faker.number.int({ min: 0, max: 9 })],
+    email: "root@gmail.com",
+    name: "root",
+    surname: "root",
+    phoneNumber: '123456789',
+    profilePicture: faker.internet.avatar(),
+    password: 'e664a81487c6df66eba27e14f9a0de04eddc5ddb114c4ac27cbb216f6732ae4d',
+    salt: 'E56sVLEqLT6C5eVT+rB3iwiwr7lK4nYUb3E4e6Jps8NeKXrjraNxN7/zIGSLczEXgyjA5uXpP11gaxBRiwJ7gI0WHOyV50YSxxYVhtooZjKwvLwYz30Zw6oE2aJK0705IKO4pJsNBblhSOmOis611JH0661socI2yHRNy3yR3u4=',
+    sessionToken: null,
+  })
+
   await knex('users').del();
   await knex('users').insert(users);
 }
